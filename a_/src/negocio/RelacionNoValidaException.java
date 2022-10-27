21966c605a1149c4c969d52321fd0a2fd80854fa
Matias <matiasmepullan@gmail.com>
2022-10-27 14:20:16 -0300

    gui

--- a_/src/negocio/RelacionNoValidaException.java
+++ a_/src/negocio/RelacionNoValidaException.java
@@ -8,7 +8,7 @@ public class RelacionNoValidaException extends RuntimeException {
 	 */
 	public RelacionNoValidaException() {
 		super();
-		// TODO Auto-generated constructor stub
+
 	}
 
 	public RelacionNoValidaException(String message) {
